f32626e5f78bf1698035b9badcd3eddb2b1400d6
devAon <yewon9742@gmail.com>
2020-07-26 02:06:59 +0900

    [ADD] HelloController 및 TDD 작성

---
+++ src/test/java/com/devAon/aoneemall/web/HelloControllerTest.java
@@ -0,0 +1,35 @@
+package com.devAon.aoneemall.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by qwone4@gmail.com on 2020-07-26
+ * Blog : https://velog.io/@aonee
+ * Github : http://github.com/devAon
+ */
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
