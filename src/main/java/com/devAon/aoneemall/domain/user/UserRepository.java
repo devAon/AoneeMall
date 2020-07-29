96c8e6ab8f2936ba2583bea603d3d292800cd036
devAon <yewon9742@gmail.com>
2020-07-29 16:08:25 +0900

    [ADD] UserRepository 작성

--- src/main/java/com/devAon/aoneemall/domain/user/UserRepository.java
+++ src/main/java/com/devAon/aoneemall/domain/user/UserRepository.java
@@ -1,5 +1,9 @@
 package com.devAon.aoneemall.domain.user;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
 /**
  * Created by qwone4@gmail.com on 2020-07-29
  * Blog : https://velog.io/@aonee
@@ -7,5 +11,6 @@ package com.devAon.aoneemall.domain.user;
  */
 
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail (String email);
 }
