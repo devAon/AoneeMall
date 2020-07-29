988bbcb2fd7a43079c014279b348aaedec4bdcc0
devAon <yewon9742@gmail.com>
2020-07-30 00:33:04 +0900

    [ADD] helloControllerTest 실패 해결

--- src/test/java/com/devAon/aoneemall/web/HelloControllerTest.java
+++ src/test/java/com/devAon/aoneemall/web/HelloControllerTest.java
@@ -1,9 +1,12 @@
 package com.devAon.aoneemall.web;
 
+import com.devAon.aoneemall.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,7 +26,11 @@ import static org.hamcrest.Matchers.is;
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
