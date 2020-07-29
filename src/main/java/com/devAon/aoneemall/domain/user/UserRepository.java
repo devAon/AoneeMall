5f9152a3c95d93a2de4fcd92c35c1abd9f1438b1
devAon <yewon9742@gmail.com>
2020-07-29 16:08:14 +0900

    [ADD] UserRepository 작성

---
+++ src/main/java/com/devAon/aoneemall/domain/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.devAon.aoneemall.domain.user;
+
+/**
+ * Created by qwone4@gmail.com on 2020-07-29
+ * Blog : https://velog.io/@aonee
+ * Github : http://github.com/devAon
+ */
+
+
+public interface UserRepository {
+}
